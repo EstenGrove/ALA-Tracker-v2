9d649c04a3994ab583f41c3de208830c4e6a778f
EstenGrove <estengrove99@gmail.com>
2020-02-12 13:32:51 -0700

    SUBJECT: Added "non-implemented"/"tested" <PastDueUpdateForm/>
    Description:
      - Built out a working version of the <PastDueUpdateForm/> including:
        - Changing tasks, updating task record, viewing ALL records for the
        current resident.
      - Component Was built in a separate environment and may be missing some
      helpers that are needed.
    
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	modified:   src/components/pastdue/PastDueUpdateForm.js
    	modified:   src/components/shared/PortableForm.js
    	modified:   src/css/pastdue/PastDueUpdateForm.module.scss
    	renamed:    src/css/pastdue/PortableForm.module.scss -> src/css/shared/PortableForm.module.scss
    	modified:   src/helpers/utils_pastdue.js
    	modified:   src/helpers/utils_styles.js

--- src/helpers/utils_pastdue.js
+++ src/helpers/utils_pastdue.js
@@ -1,14 +1,14 @@
 import { test } from "./utils_env";
 import { pastDue } from "./utils_endpoints";
 import {
-	format,
-	subDays,
-	startOfDay,
-	endOfDay,
-	startOfWeek,
-	endOfWeek,
-	startOfMonth,
-	endOfMonth
+  format,
+  subDays,
+  startOfDay,
+  endOfDay,
+  startOfWeek,
+  endOfWeek,
+  startOfMonth,
+  endOfMonth
 } from "date-fns";
 import { isEmptyArray } from "./utils_types";
 
@@ -22,103 +22,103 @@ import { isEmptyArray } from "./utils_types";
  * @param {number} rows - The number of rows(count) from fetch from the database.
  */
 const getCommunityPastDue = async (
-	token,
-	facilityID,
-	startDate = subDays(new Date(), 2),
-	endDate = new Date(),
-	index = 0,
-	rows = 25
+  token,
+  facilityID,
+  startDate = subDays(new Date(), 2),
+  endDate = new Date(),
+  index = 0,
+  rows = 25
 ) => {
-	let url = test.base + pastDue.get;
-	url += "?facilityId=" + facilityID + "&";
-	url += new URLSearchParams({
-		startDate: format(startDate, "MM/DD/YYYY"),
-		endDate: format(endDate, "MM/DD/YYYY"),
-		index: index,
-		rows: rows
-	});
+  let url = test.base + pastDue.get;
+  url += "?facilityId=" + facilityID + "&";
+  url += new URLSearchParams({
+    startDate: format(startDate, "MM/DD/YYYY"),
+    endDate: format(endDate, "MM/DD/YYYY"),
+    index: index,
+    rows: rows
+  });
 
-	try {
-		const request = await fetch(url, {
-			method: "GET",
-			headers: {
-				Authorization: "Basic " + btoa(test.user + ":" + test.password),
-				SecurityToken: token,
-				"Content-Type": "application/json"
-			}
-		});
-		const response = await request.json();
-		return response.Data;
-	} catch (err) {
-		console.log("An error occurred ", err);
-		return err.message;
-	}
+  try {
+    const request = await fetch(url, {
+      method: "GET",
+      headers: {
+        Authorization: "Basic " + btoa(test.user + ":" + test.password),
+        SecurityToken: token,
+        "Content-Type": "application/json"
+      }
+    });
+    const response = await request.json();
+    return response.Data;
+  } catch (err) {
+    console.log("An error occurred ", err);
+    return err.message;
+  }
 };
 
 // fetches the past due records for ONLY a specific day (start of day - end of day)
 const getDailyPastDue = async (
-	token,
-	facilityID,
-	startDate = startOfDay(new Date()),
-	endDate = endOfDay(new Date()),
-	index = 0,
-	rows = 25
+  token,
+  facilityID,
+  startDate = startOfDay(new Date()),
+  endDate = endOfDay(new Date()),
+  index = 0,
+  rows = 25
 ) => {
-	const dayStart = format(startDate, "MM/DD/YYYY");
-	const dayEnd = format(endDate, "MM/DD/YYYY");
-	const dailyRecords = await getCommunityPastDue(
-		token,
-		facilityID,
-		dayStart,
-		dayEnd,
-		index,
-		rows
-	);
-	return dailyRecords;
+  const dayStart = format(startDate, "MM/DD/YYYY");
+  const dayEnd = format(endDate, "MM/DD/YYYY");
+  const dailyRecords = await getCommunityPastDue(
+    token,
+    facilityID,
+    dayStart,
+    dayEnd,
+    index,
+    rows
+  );
+  return dailyRecords;
 };
 
 // fetches records ONLY for a specific week-long range.
 const getWeeklyPastDue = async (
-	token,
-	facilityID,
-	startDate = startOfWeek(new Date()),
-	endDate = endOfWeek(new Date()),
-	index = 0,
-	rows = 25
+  token,
+  facilityID,
+  startDate = startOfWeek(new Date()),
+  endDate = endOfWeek(new Date()),
+  index = 0,
+  rows = 25
 ) => {
-	const weekStart = format(startDate, "MM/DD/YYYY");
-	const weekEnd = format(endDate, "MM/DD/YYYY");
-	const weekRecords = await getCommunityPastDue(
-		token,
-		facilityID,
-		weekStart,
-		weekEnd,
-		index,
-		rows
-	);
-	return weekRecords;
+  const weekStart = format(startDate, "MM/DD/YYYY");
+  const weekEnd = format(endDate, "MM/DD/YYYY");
+  const weekRecords = await getCommunityPastDue(
+    token,
+    facilityID,
+    weekStart,
+    weekEnd,
+    index,
+    rows
+  );
+  return weekRecords;
 };
 
 // fetches records ONLY for a specific month
 const getMonthlyPastDue = async (
-	token,
-	facilityID,
-	startDate = startOfMonth(new Date()),
-	endDate = endOfMonth(new Date()),
-	index = 0,
-	rows = 25
+  token,
+  facilityID,
+  startDate = startOfMonth(new Date()),
+  endDate = endOfMonth(new Date()),
+  index = 0,
+  rows = 25
 ) => {
-	const monthStart = format(startDate, "MM/DD/YYYY");
-	const monthEnd = format(endDate, "MM/DD/YYYY");
-	const monthRecords = await getCommunityPastDue(
-		token,
-		facilityID,
-		monthStart,
-		monthEnd,
-		index,
-		rows
-	);
-	return monthRecords;
+  const monthStart = format(startDate, "MM/DD/YYYY");
+  const monthEnd = format(endDate, "MM/DD/YYYY");
+  const monthRecords = await getCommunityPastDue(
+    token,
+    facilityID,
+    monthStart,
+    monthEnd,
+    index,
+    rows
+  );
+  return monthRecords;
 };
 
 /////////////////////////////////////////
@@ -127,12 +127,12 @@ const getMonthlyPastDue = async (
 
 // sorts records by resident last name alphabetically.
 const sortPastDueRecords = records => {
-	if (isEmptyArray(records)) return [];
-	return records.sort((a, b) => {
-		return a.Resident[0].ResidentLastName.localeCompare(
-			b.Resident[0].ResidentLastName
-		);
-	});
+  if (isEmptyArray(records)) return [];
+  return records.sort((a, b) => {
+    return a.Resident[0].ResidentLastName.localeCompare(
+      b.Resident[0].ResidentLastName
+    );
+  });
 };
 
 // counts records for each resident and returns an object with residentIDs as keys and record counts as values.
@@ -144,14 +144,14 @@ const sortPastDueRecords = records => {
  * @example countPastDueRecords(records, x => x.Resident[0].ResidentLastName)
  */
 const countPastDuePerResident = (records, iteratee) => {
-	return records.reduce((all, item) => {
-		const keyToGroupBy = iteratee(item);
-		if (!item[keyToGroupBy]) {
-			all[keyToGroupBy] = {};
-		}
-		all[keyToGroupBy] = item.PastDueScheduleTask.length;
-		return all;
-	}, {});
+  return records.reduce((all, item) => {
+    const keyToGroupBy = iteratee(item);
+    if (!item[keyToGroupBy]) {
+      all[keyToGroupBy] = {};
+    }
+    all[keyToGroupBy] = item.PastDueScheduleTask.length;
+    return all;
+  }, {});
 };
 
 /**
@@ -160,31 +160,55 @@ const countPastDuePerResident = (records, iteratee) => {
  * @param {array} records - An array of Past Due task records.
  */
 const getTotalPastDueCount = records => {
-	if (isEmptyArray(records)) return 0;
-	return records.reduce((all, entry) => {
-		const { length } = entry.PastDueScheduleTask;
-		all += length;
-		return all;
-	}, 0);
+  if (isEmptyArray(records)) return 0;
+  return records.reduce((all, entry) => {
+    const { length } = entry.PastDueScheduleTask;
+    all += length;
+    return all;
+  }, 0);
+};
+
+// gets the total count of past due records for a single resident
+const getPastDueCount = residentRecord => {
+  if (isEmptyArray(residentRecord.PastDueScheduleTask))
+    return "No past due records";
+  return residentRecord.PastDueScheduleTask.length + " records found.";
+};
+
+// gets the total count for a single resident WITHOUT any formatting
+const getPastDueCountRaw = residentRecord => {
+  if (isEmptyArray(residentRecord.PastDueScheduleTask)) return 0;
+  return residentRecord.PastDueScheduleTask.length;
 };
 
 // formats resident full name (ie First Last)
 const getResidentNamePastDue = record => {
-	const { ResidentFirstName, ResidentLastName } = record.Resident[0];
-	return `${ResidentFirstName} ${ResidentLastName}`;
+  const { ResidentFirstName, ResidentLastName } = record.Resident[0];
+  return `${ResidentFirstName} ${ResidentLastName}`;
+};
+
+// STRINGIFIES TASK RECORD AND DEEP COMPARES VALUE EQUALITY "NOT" REFERENTIAL EQUALITY
+const isActiveTask = (active, task) => {
+  const strActive = JSON.stringify(active);
+  const strTask = JSON.stringify(task);
+  if (strActive === strTask) return true;
+  return false;
 };
 
 // FETCHING PAST DUE RECORDS
 export {
-	getCommunityPastDue,
-	getDailyPastDue,
-	getWeeklyPastDue,
-	getMonthlyPastDue
+  getCommunityPastDue,
+  getDailyPastDue,
+  getWeeklyPastDue,
+  getMonthlyPastDue
 };
 
 export {
-	sortPastDueRecords,
-	countPastDuePerResident,
-	getTotalPastDueCount,
-	getResidentNamePastDue
+  sortPastDueRecords,
+  countPastDuePerResident,
+  getTotalPastDueCount,
+  getResidentNamePastDue,
+  getPastDueCountRaw,
+  getPastDueCount,
+  isActiveTask
 };
