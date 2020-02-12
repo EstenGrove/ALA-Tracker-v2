082216360d06ec35f411b04c7e81bb18057434b6
Steven G <estengrove99@gmail.com>
2020-02-12 06:17:59 -0700

    SUBJECT: Progress made on <PastDueUpdateForm/> .
    	Description:
    
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	modified:   src/components/pastdue/PastDueEntry.js
    	modified:   src/components/pastdue/PastDueUpdateForm.js
    	modified:   src/css/pastdue/PortableForm.module.scss
    	modified:   src/helpers/utils_tasks.js

--- src/components/pastdue/PastDueUpdateForm.js
+++ src/components/pastdue/PastDueUpdateForm.js
@@ -6,11 +6,11 @@ import { STATUS as statuses } from "../../helpers/utils_options";
 import { useForm } from "../../utils/useForm";
 import { getUserInitials } from "../../helpers/utils_processing";
 import {
-	getTaskDescription,
-	getTaskStatus,
-	getTaskDueDate,
-	getTaskCategory,
-	checkForNotes
+  getTaskDescription,
+  getTaskStatus,
+  getTaskDueDate,
+  getTaskCategory,
+  checkForNotes
 } from "../../helpers/utils_tasks";
 import { themeColors, adlColors, adlIcons } from "../../helpers/utils_styles";
 import PortableForm from "../shared/PortableForm";
@@ -33,74 +33,72 @@ import DropdownSelect from "../shared/DropdownSelect";
 // 7. Description (secondary details)
 
 const PastDueUpdateForm = ({ activeTask = {}, records = [] }) => {
-	const {
-		formState,
-		handleChange,
-		handleCheckbox,
-		handleBlur,
-		handleReset
-	} = useForm({
-		pastDueStatus: activeTask?.TaskStatus,
-		pastDuePriority: activeTask?.PriorityName
-	});
+  const {
+    formState,
+    handleChange,
+    handleCheckbox,
+    handleBlur,
+    handleReset
+  } = useForm({
+    pastDueStatus: activeTask?.TaskStatus,
+    pastDuePriority: activeTask?.PriorityName
+  });
 
-	const { values } = formState;
-	return (
-		<>
-			<article className={styles.PastDueUpdateForm}>
-				<h2
-					className={styles.PastDueUpdateForm_title}
-					style={{ color: adlColors[getTaskCategory(activeTask)] }}
-				>
-					<svg className={styles.PastDueUpdateForm_icon}>
-						<use
-							fill={`${adlIcons[getTaskCategory(activeTask)].styles.fill}`}
-							xlinkHref={`${sprite}#icon-${
-								adlIcons[getTaskCategory(activeTask)].icon
-							}`}
-						></use>
-					</svg>
-					{getTaskCategory(activeTask)}
-				</h2>
-				<section className={styles.PastDueUpdateForm_details}>
-					<h4 className={styles.PastDueUpdateForm_details_heading}>
-						Description
-					</h4>
-					<p>{getTaskDescription(activeTask)}</p>
-					<h4 className={styles.PastDueUpdateForm_details_heading}>
-						Additional Notes
-					</h4>
-					<p>{checkForNotes(activeTask, "No notes")}</p>
-					<h4 className={styles.PastDueUpdateForm_details_heading}>Due Date</h4>
-					<p style={{ color: themeColors.main.red }}>
-						{getTaskDueDate(activeTask)}
-					</p>
-					<p>
-						Created by{" "}
-						<b style={{ color: themeColors.main.blue, fontStyle: "italic" }}>
-							{getUserInitials(activeTask)}
-						</b>
-					</p>
-				</section>
+  const { values } = formState;
+  return (
+    <>
+      <article className={styles.PastDueUpdateForm}>
+        <h2
+          className={styles.PastDueUpdateForm_title}
+          style={{ color: adlColors[getTaskCategory(activeTask)] }}
+        >
+          <svg className={styles.PastDueUpdateForm_icon}>
+            <use
+              fill={`${adlIcons[getTaskCategory(activeTask)].styles.fill}`}
+              xlinkHref={`${sprite}#icon-${adlIcons[getTaskCategory(activeTask)].icon}`}
+            ></use>
+          </svg>
+          {getTaskCategory(activeTask)}
+        </h2>
+        <section className={styles.PastDueUpdateForm_details}>
+          <h4 className={styles.PastDueUpdateForm_details_heading}>
+            Description
+          </h4>
+          <p>{getTaskDescription(activeTask)}</p>
+          <h4 className={styles.PastDueUpdateForm_details_heading}>
+            Additional Notes
+          </h4>
+          <p>{checkForNotes(activeTask, "No notes")}</p>
+          <h4 className={styles.PastDueUpdateForm_details_heading}>Due Date</h4>
+          <p style={{ color: themeColors.main.red }}>
+            {getTaskDueDate(activeTask)}
+          </p>
+          <p>
+            Created by{" "}
+            <b style={{ color: themeColors.main.blue, fontStyle: "italic" }}>
+              {getUserInitials(activeTask)}
+            </b>
+          </p>
+        </section>
 
-				<section className={styles.PastDueUpdateForm_inner}>
-					<PortableForm
-						handleBlur={handleBlur}
-						handleChange={handleChange}
-						handleCheckbox={handleCheckbox}
-					>
-						<DropdownSelect
-							val={values.pastDueStatus}
-							name="pastDueStatus"
-							id="pastDueStatus"
-							label="Edit Status"
-							options={statuses}
-						/>
-					</PortableForm>
-				</section>
-			</article>
-		</>
-	);
+        <section className={styles.PastDueUpdateForm_inner}>
+          <PortableForm
+            handleBlur={handleBlur}
+            handleChange={handleChange}
+            handleCheckbox={handleCheckbox}
+          >
+            <DropdownSelect
+              val={values.pastDueStatus}
+              name="pastDueStatus"
+              id="pastDueStatus"
+              label="Edit Status"
+              options={statuses}
+            />
+          </PortableForm>
+        </section>
+      </article>
+    </>
+  );
 };
 
 export default PastDueUpdateForm;
@@ -108,6 +106,6 @@ export default PastDueUpdateForm;
 PastDueUpdateForm.defaultProps = {};
 
 PastDueUpdateForm.propTypes = {
-	activeTask: PropTypes.object.isRequired,
-	records: PropTypes.arrayOf(PropTypes.object)
+  activeTask: PropTypes.object.isRequired,
+  records: PropTypes.arrayOf(PropTypes.object)
 };
